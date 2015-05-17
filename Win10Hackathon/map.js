b058d3038bc0f20de5a79ce9a4cf68f1b85c545c
Cathy Li <licathy06@gmail.com>
2015-05-17 13:06:06 -0400

    Multiple markers

--- Win10Hackathon/map.js
+++ Win10Hackathon/map.js
@@ -38,12 +38,95 @@ function initialize() {
     document.getElementById("startTracking").addEventListener("click",
             defaultZoomMap);
 
+}
 
+var coord2DArray = [];
+
+function parseCoord() {
+    var latTemp;
+    var longTemp;
+    var j = 0;
+    var str;
+    //var coord2DArray = [];
+    var coordArray = loadCoord();
+    console.log("coordArray length: " + coordArray.length);
+    for (i = 0; i < coordArray.length; i++) {
+        coord2DArray[i] = new Array(2);
+    }
+    console.log("length " + coordArray.length);
+    var count = 0;
+
+    for (k = 0; k < coordArray.length; k++) {
+        j = 0;
+        str = coordArray[k].childNodes[0].nodeValue.split(",");
+        coord2DArray[k][j] = str[1];
+        coord2DArray[k][j + 1] = str[0];
+        count = count + 1;
+        //console.log(i + " lat " + coord2DArray[k][j]);
+        //console.log(i + " long " + coord2DArray[k][j+1]);
+    }
+    /*console.log("after parsing, " + coord2DArray[0][0]);
+    console.log("after parsing, " + coord2DArray[0][1]);
+    console.log("after parsing, " + coord2DArray[1][0]);
+    console.log("after parsing, " + coord2DArray[1][1]);
+    console.log("after parsing, " + coord2DArray[2][0]);
+    console.log("after parsing, " + coord2DArray[2][1]);
+    console.log("count " + count);*/
+    //return coord2DArray;
+}
+
+//returns description for current coordinate index in array
+function getCurDescr(int) {
+    var dArray = loadDescr();
+    return dArray[int].childNodes[0].nodeValue;
+}
+
+//load xml file then return two arrays, coordinate and descriptions
+function loadCoord() {
+    function loadXMLDoc(filename) {
+        if (window.XMLHttpRequest) {
+            var xmlhttp = new XMLHttpRequest();
+        }
+        else {
+            var xmlhttp = new ACtiveXObject("Microsoft.XMLTTP");
+        }
+        xmlhttp.open("GET", filename, false);
+        xmlhttp.send();
+        console.log("test 1");
+        return xmlhttp.responseXML;
+    }
+
+    var xmlDoc = loadXMLDoc("https://dl.dropboxusercontent.com/u/3137747/parking_meter_rates_and_time_limits.xml");
+    console.log("test 2");
+
+    var coord = xmlDoc.getElementsByTagName("coordinates");
+    return coord;
+}
+
+function loadDescr() {
+    function loadXMLDoc(filename) {
+        if (window.XMLHttpRequest) {
+            var xmlhttp = new XMLHttpRequest();
+        }
+        else {
+            var xmlhttp = new ACtiveXObject("Microsoft.XMLTTP");
+        }
+        xmlhttp.open("GET", filename, false);
+        xmlhttp.send();
+        console.log("test 1");
+        return xmlhttp.responseXML;
+    }
+
+    var xmlDoc = loadXMLDoc("https://dl.dropboxusercontent.com/u/3137747/parking_meter_rates_and_time_limits.xml");
+    console.log("test 2");
+
+    var descr = xmlDoc.getElementsByTagName("description");
+    return descr;
 }
 
 function defaultZoomMap() {
     sleep(4000);
-    var newCenter = new google.maps.LatLng(49.276842034114196, -122.9143051590263)
+    var newCenter = new google.maps.LatLng(49.276842034114196, -122.9143051590263);
     map.setCenter(newCenter);
     var marker = new google.maps.Marker({
         map: map,
@@ -51,6 +134,52 @@ function defaultZoomMap() {
     });
     map.setZoom(14);
     map.panTo(marker.position);
+}
+
+function findNearPoints(lat1, lon1) {
+    //var coord2DArray = parseCoord();
+    parseCoord();
+    var count = 0;
+    var myNearPoints = [];
+    //console.log(coord2DArray.length);
+    //console.log(coord2DArray[0,0]);
+    //console.log(coord2DArray[1,0]);
+    for (i = 0; i < coord2DArray.length; i++) {
+        // console.log(coord2DArray[i][1]-lon1);
+        if (count < 20) {
+            //if (coord2DArray[i][0] - lat1 < 0.005 || coord2DArray[i][0] - lat1 > 0.005) {
+            // if (coord2DArray[i][1] - lon1 < 0.005 || coord2DArray[i][1] - lon1 > 0.005) {
+            if(Math.abs(lat1+lon1-coord2DArray[i][0]-coord2DArray[i][1]) < 0.0005) {
+                    myNearPoints.push(coord2DArray[i][0]);
+                    myNearPoints.push(coord2DArray[i][1]);
+                    count = count + 1;
+                    //console.log("count " + count);
+                //}
+            }
+        }
+       // console.log("my while loop is executing");
+    }
+    var marker;
+    console.log(myNearPoints[0]);
+    console.log(myNearPoints[1]);
+    console.log(myNearPoints[2]);
+    console.log(myNearPoints[3]);
+    console.log(myNearPoints[4]);
+    console.log(myNearPoints[5]);
+    for (j = 0; j < myNearPoints.length; j++) {
+        var lat = myNearPoints[j];
+        var long = myNearPoints[j + 1];
+        //console.log(lat);
+        //console.log(long);
+        var newCenter = new google.maps.LatLng(lat, long);
+        marker = new google.maps.Marker({
+            map:map,
+            position: newCenter
+        });
+        j = j + 1;
+        //map.panTo(marker.position);
+       //console.log("adding markers");
+    }
 }
 
 function buttonClickHandler(eventInfo) {
@@ -83,6 +212,7 @@ function addMarkers() {
             map: map
             //icon: getCircle(earthquake.properties.mag)
         });
+
     }
 }
 
@@ -114,6 +244,7 @@ function getLocation() {
             var lat = results[0].geometry.location.lat();
             var lng = results[0].geometry.location.lng();
             zoomMap(lat, lng);
+            findNearPoints(lat, lng);
         }
         else {
             alert("Request failed.")
@@ -127,7 +258,9 @@ function zoomMap(lat, long) {
     map.setCenter(newCenter);
     var marker = new google.maps.Marker({
         map: map,
-        position: newCenter
+        position: newCenter,
+        animation: google.maps.Animation.DROP,
+        icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
     });
     map.setZoom(14);
     map.panTo(marker.position);
