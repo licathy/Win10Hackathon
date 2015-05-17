5ec14a70303d246144f7bc0295c0d20c047e8fda
Yolanda Wong <yw.yunwing@gmail.com>
2015-05-17 06:06:38 -0700

    Adding more functions to xml.js

--- Win10Hackathon/js/xml.js
+++ Win10Hackathon/js/xml.js
@@ -24,12 +24,12 @@
 
 	}*/
 
-	function parseCoord(coordArray) {
+	function parseCoord() {
 	    var latTemp;
 	    var longTemp;
 	    var j = 0;
 	    var str;
-	    var coord2DArray = new Array(2);
+	    var coord2DArray = loadCoord();
 	    for (i = 0; i < coordArray.length; i++) {
 	        coord2DArray[i] = new Array(2);
 	    }
@@ -49,8 +49,14 @@
 	    return coord2DArray;
 	}
 
+    //returns description for current coordinate index in array
+	function getCurDescr(int) {
+	    var dArray = loadDescr();
+	    return dArray[int].childNodes[0].nodeValue;
+	}
+
     //load xml file then return two arrays, coordinate and descriptions
-	function load() {
+	function loadCoord() {
 	    function loadXMLDoc(filename) {
 	        if (window.XMLHttpRequest) {
 	            var xmlhttp = new XMLHttpRequest();
@@ -68,10 +74,10 @@
 	   console.log("test 2");
 
 
-	   var outCoord = "";
-	   var outDescr = "";
-	   var cArray = new Array();
-	   var dArray = new Array();
+	   //var outCoord = "";
+	   //var outDescr = "";
+	   //var cArray = new Array();
+	   //var dArray = new Array();
 	   //cArray = xmlDoc.getElementsByTagName("coordinates");
 	   //dArray = xmlDoc.getElementsByTagName("description");
 	  // var coordArray = cArray;
@@ -81,12 +87,12 @@
 	   var coord = xmlDoc.getElementsByTagName("coordinates");
 	   var descr = xmlDoc.getElementsByTagName("description");
 
-	   cArray = parseCoord(coord);
-	   //console.log("length of array: " + coordArray.length);
+	  // cArray = parseCoord(coord);
+	   //console.log("length of array: " + cArray.length);
 
-	   var latTemp;
-	   var longTemp;
-	   var j = 0;
+	   //var latTemp;
+	   //var longTemp;
+	   //var j = 0;
 	   //coordArray = cArray;
 
 	   /*for (var i = 0; i < coordArray.length; i++) {
@@ -97,9 +103,55 @@
 	       console.log(i + " long " + longTemp);
 	   }*/
 	   console.log("done");
+	   return coord;
 	}
 
+	function loadDescr() {
+	    function loadXMLDoc(filename) {
+	        if (window.XMLHttpRequest) {
+	            var xmlhttp = new XMLHttpRequest();
+	        }
+	        else {
+	            var xmlhttp = new ACtiveXObject("Microsoft.XMLTTP");
+	        }
+	        xmlhttp.open("GET", filename, false);
+	        xmlhttp.send();
+	        console.log("test 1");
+	        return xmlhttp.responseXML;
+	    }
+
+	    var xmlDoc = loadXMLDoc("https://dl.dropboxusercontent.com/u/3137747/parking_meter_rates_and_time_limits.xml");
+	    console.log("test 2");
 
 
-	app.start();
+	    //var outCoord = "";
+	    //var outDescr = "";
+	    //var cArray = new Array();
+	    //var dArray = new Array();
+	    //cArray = xmlDoc.getElementsByTagName("coordinates");
+	    //dArray = xmlDoc.getElementsByTagName("description");
+	    // var coordArray = cArray;
+	    //var descrArray = dArray;
+
+
+	    var descr = xmlDoc.getElementsByTagName("description");
+
+	    // cArray = parseCoord(coord);
+	    //console.log("length of array: " + cArray.length);
+
+	    //var latTemp;
+	    //var longTemp;
+	    //var j = 0;
+	    //coordArray = cArray;
+
+	    /*for (var i = 0; i < coordArray.length; i++) {
+            j = 0;
+            latTemp = coordArray[i, j];
+            longTemp = coordArray[i, j + 1];
+            console.log(i + " lat " + latTemp);
+            console.log(i + " long " + longTemp);
+        }*/
+	    console.log("done");
+	    return descr;
+	}
 })();
