269b97358b9623786fd530ddcae53b3f7727c245
Yolanda Wong <yw.yunwing@gmail.com>
2015-05-17 06:13:29 -0700

    Cleaned up xml.js

--- Win10Hackathon/js/xml.js
+++ Win10Hackathon/js/xml.js
@@ -2,27 +2,6 @@
 // http://go.microsoft.com/fwlink/?LinkId=232509
 (function () {
 	"use strict";
-	/*function parseCoordStruct(coordArray) {
-
-	    var long_lat = { longitude: 0, latitude: 0 };
-	    var coord2DArray = new Array();
-	    var tempArray = new Array(Object);
-	    var str;
-
-	    for (var i = 0; i < coordArray.length; i++) {
-
-	        str = coordArray[i].childNodes[0].nodeValue.split(",");
-	        long_lat.longitude = str[0];
-	        long_lat.latitude = str[1];
-	        console.log(long_lat.longitude);
-	        console.log(long_lat.latitude);
-	        tempArray[long_lat];
-	        coord2DArray = coord2DArray.concat(tempArray);
-	    }
-        
-	    return coord2DArray;
-
-	}*/
 
 	function parseCoord() {
 	    var latTemp;
@@ -73,36 +52,7 @@
 	   var xmlDoc = loadXMLDoc("https://dl.dropboxusercontent.com/u/3137747/parking_meter_rates_and_time_limits.xml");
 	   console.log("test 2");
 
-
-	   //var outCoord = "";
-	   //var outDescr = "";
-	   //var cArray = new Array();
-	   //var dArray = new Array();
-	   //cArray = xmlDoc.getElementsByTagName("coordinates");
-	   //dArray = xmlDoc.getElementsByTagName("description");
-	  // var coordArray = cArray;
-	   //var descrArray = dArray;
-
-
 	   var coord = xmlDoc.getElementsByTagName("coordinates");
-	   var descr = xmlDoc.getElementsByTagName("description");
-
-	  // cArray = parseCoord(coord);
-	   //console.log("length of array: " + cArray.length);
-
-	   //var latTemp;
-	   //var longTemp;
-	   //var j = 0;
-	   //coordArray = cArray;
-
-	   /*for (var i = 0; i < coordArray.length; i++) {
-	       j = 0;
-	       latTemp = coordArray[i, j];
-	       longTemp = coordArray[i, j + 1];
-	       console.log(i + " lat " + latTemp);
-	       console.log(i + " long " + longTemp);
-	   }*/
-	   console.log("done");
 	   return coord;
 	}
 
@@ -123,35 +73,7 @@
 	    var xmlDoc = loadXMLDoc("https://dl.dropboxusercontent.com/u/3137747/parking_meter_rates_and_time_limits.xml");
 	    console.log("test 2");
 
-
-	    //var outCoord = "";
-	    //var outDescr = "";
-	    //var cArray = new Array();
-	    //var dArray = new Array();
-	    //cArray = xmlDoc.getElementsByTagName("coordinates");
-	    //dArray = xmlDoc.getElementsByTagName("description");
-	    // var coordArray = cArray;
-	    //var descrArray = dArray;
-
-
 	    var descr = xmlDoc.getElementsByTagName("description");
-
-	    // cArray = parseCoord(coord);
-	    //console.log("length of array: " + cArray.length);
-
-	    //var latTemp;
-	    //var longTemp;
-	    //var j = 0;
-	    //coordArray = cArray;
-
-	    /*for (var i = 0; i < coordArray.length; i++) {
-            j = 0;
-            latTemp = coordArray[i, j];
-            longTemp = coordArray[i, j + 1];
-            console.log(i + " lat " + latTemp);
-            console.log(i + " long " + longTemp);
-        }*/
-	    console.log("done");
 	    return descr;
 	}
 })();
